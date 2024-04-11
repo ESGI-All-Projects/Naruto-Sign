b15d6274ef8492ba135a3d51dd32e090b4c34537
JulianALZ <julian.alizay@gmail.com>
2024-04-11 22:56:16 +0200

    final commit front & api

--- src/front/web_app.py
+++ src/front/web_app.py
@@ -1,18 +1,17 @@
-from flask import Flask, render_template, Response
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 import cv2
-from fastai.vision.all import load_learner
 import base64
-import requests
-import numpy as np
+import httpx
+import asyncio
 
-app = Flask(__name__, template_folder='templates')  # Assurez-vous que le chemin est correct
+app = Flask(__name__, template_folder='templates')
 socketio = SocketIO(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Page HTML pour l'interface utilisateur
+    return render_template('index.html')
 
 
 def gen_frames():
@@ -25,41 +24,25 @@ def gen_frames():
             _, buffer = cv2.imencode('.jpg', frame)
             frame_base64 = base64.b64encode(buffer).decode('utf-8')
 
-            processed_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # Convertir le frame pour l'envoyer comme fichier
+            frame_bytes = buffer.tobytes()
 
-
-            # pred_class, pred_idx, probs = model.predict(processed_frame)
-            # Envoi de l'image au serveur FastAPI pour la prédiction
-            response = requests.post('http://localhost:9090/predict/',
-            files={"file": ('image.jpg', buffer.tobytes(), 'image/jpeg')})
-
-
-            class_name = str(pred_class)
-            max_prob = probs[pred_idx].item() * 100
+            class_name, max_prob = asyncio.run(predict_async(frame_bytes))
 
             socketio.emit('stream_response',
                           {'image': frame_base64, 'class': class_name, 'probability': f"{max_prob:.2f}%"})
 
-    cap.release()
-    #     else:
-    #         # Encodage de l'image en format compatible pour l'envoi via HTTP
-    #         _, buffer = cv2.imencode('.jpg', frame)
-    #         frame_encoded = base64.b64encode(buffer).decode('utf-8')
-    #
-    #         # Envoi de l'image au serveur FastAPI pour la prédiction
-    #         response = requests.post('http://localhost:9090/predict/',
-    #                                  files={"file": ('image.jpg', buffer.tobytes(), 'image/jpeg')})
-    #         if response.status_code == 200:
-    #             prediction = response.json()['prediction']
-    #             class_name = prediction[0]
-    #             probability = prediction[
-    #                               2].max().item() * 100  # Assurez-vous que le format de réponse correspond à ce que vous attendez
-    #
-    #         # Utilisez les résultats de la prédiction pour mettre à jour votre interface, etc.
-    #         socketio.emit('stream_response',
-    #                       {'image': frame_encoded, 'class': class_name, 'probability': f"{probability:.2f}%"})
-    #
-    # cap.release()
+
+async def predict_async(image_bytes):
+    url = 'http://localhost:9090/predict/'
+    files = {'file': ('image.jpg', image_bytes, 'image/jpeg')}
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(url, files=files)
+        if resp.status_code == 200:
+            return resp.json()
+        else:
+            print(f"Erreur de prédiction: {resp.status_code}")
+            return None
 
 
 @socketio.on('start_camera')
@@ -69,4 +52,3 @@ def handle_start_camera(json):
 
 if __name__ == '__main__':
     socketio.run(app, port=8080, debug=True, allow_unsafe_werkzeug=True)
-
--- src/model_api/prediction.py
+++ src/model_api/prediction.py
@@ -1,35 +1,27 @@
 from fastapi import FastAPI, File, UploadFile
 from fastai.vision.all import load_learner
 
-# Chemin vers le modèle
-model_path = "/models/fastai-v1.pth"
 
-# Charger le modèle une fois au démarrage de l'application
+model_path = "./models/fastai-v1.pth"
 model = load_learner(model_path)
-
-# Initialiser l'application FastAPI
 app = FastAPI()
 
-# Endpoint pour effectuer des prédictions
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
     # Lire l'image depuis le fichier uploadé
     #contents = await file.read()
     img = await file.read()
 
-    # Charger l'image
-    #img = open_image(contents)
-
-    # Faire une prédiction avec le modèle chargé
     pred_class, pred_idx, probs = model.predict(img)
+    print(type(probs))
+    print("idx", type(pred_idx))
 
-    # Retourner la prédiction au format JSON
-    return {"pred_class": str(pred_class),
-            "pred_idx": str(pred_idx),
-            "probs": str(pred_idx)}
+    class_name = str(pred_class)
+    max_prob = probs[pred_idx].item() * 100
+
+    return class_name, max_prob
 
 
-# Point d'entrée de l'application FastAPI avec uvicorn
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=9090)
